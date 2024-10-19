0682c08b100619e3b7accd6019670ba6d72f143b
ElijahQuinones <elijahluisquoinones@gmail.com>
2024-10-19 19:10:42 -0400

    Add full daily spanish word funtionality

---
+++ readme.txt
@@ -0,0 +1,6 @@
+This Project is intended to be run on a Raspberry Pi with the Inky display from Pimoroni.
+
+This is a project meant to help someone learn spanish words every day.
+It pulls from a list of the top 10,000 most used spainsh words.
+It then sends these words to chatgpt to ask for a translation and use in a sentance.
+Finally it removes the word displayed on the Inky screen so that you will not get the same definiton twice.
--- spanishDict.py
+++ spanishDict.py
@@ -2,21 +2,94 @@
 import os
 from PIL import Image
 from inky.auto import auto
+from PIL import Image, ImageFont, ImageDraw
+from font_fredoka_one import FredokaOne
+from openai import OpenAI
 
+OPENAI_APIKEY = os.environ["OPENAI_APIKEY"]
 PATH = os.path.dirname(__file__)
 
-try:
-    inky_display = auto(ask_user=True, verbose=True)
-except TypeError:
-    raise TypeError("You Need to update the Inky library to >= v1.1.0")
+client = OpenAI(
+    api_key=OPENAI_APIKEY
+)
+# Set up the correct display and scaling factors
 
-try:
-    inky_display.set_border(inky_display.BLACK)
-except NotImplementedError:
-    pass
+inky_display = auto(ask_user=True, verbose=True)
+inky_display.set_border(inky_display.WHITE)
+# inky_display.set_rotation(180)
 
-img =Image.open(os.path.join(PATH,"images/Flag-Puerto-Rico.jpg"))
-img = img.resize(inky_display.resolution)
+def getsize(font, text):
+    _, _, right, bottom = font.getbbox(text)
+    return (right, bottom)
 
-inky_display.set_image(img)
-inky_display.show()
+
+# This function will take a quote as a string, a width to fit
+# it into, and a font (one that's been loaded) and then reflow
+# that quote with newlines to fit into the space required.
+def reflow_quote(quote, width, font):
+    words = quote.split(" ")
+    reflowed = '"'
+    line_length = 0
+
+    for i in range(len(words)):
+        word = words[i] + " "
+        word_length = getsize(font, word)[0]
+        line_length += word_length
+
+        if line_length < width:
+            reflowed += word
+        else:
+            line_length = word_length
+            reflowed = reflowed[:-1] + "\n  " + word
+
+    reflowed = reflowed.rstrip() + '"'
+
+    return reflowed
+
+WIDTH = inky_display.width
+HEIGHT = inky_display.height
+font_size = 24
+
+padding = 5
+max_width = WIDTH - padding
+font = ImageFont.truetype(FredokaOne, font_size)
+max_height = HEIGHT - padding - getsize(font, "ABCD ")[1]
+
+with open(os.path.join(PATH,"spanishWordlist.txt"),"r+") as spanishWordListFile:
+    wordOfTheday=spanishWordListFile.readline()
+    
+    definition = client.chat.completions.create(
+    model="gpt-4o-mini",
+    messages=[
+        {"role": "system", "content": "You are a spanish teacher."},
+        {
+            "role": "user",
+            "content": "Translate the word "+ wordOfTheday +" and use it in a sentence"
+        }
+    ]
+)
+    
+    
+
+    x = (WIDTH - max_width) / 2
+    y = (HEIGHT - max_height) 
+
+
+    img = Image.new("P", (WIDTH, HEIGHT))
+    draw = ImageDraw.Draw(img)
+
+
+    #hatch_spacing = 12
+
+    #for x in range(0, 2 * WIDTH, hatch_spacing):
+    #    draw.line((x, 0, x - WIDTH, HEIGHT), fill=inky_display.WHITE, width=3)
+
+
+
+    print(definition)
+    #draw.text((6, 8), "this is a test", fill ="red", font = font,  
+    #          spacing = 50, align ="right") 
+    draw.text((6,8), reflow_quote(str(definition.choices[0].message.content),600,font), fill=inky_display.WHITE, font=font, spacing=padding,  align="left")
+    inky_display.set_image(img)
+    inky_display.show()
+    os.system("tail -n +2 spanishWordlist.txt > tmpfile.txt  && mv  tmpfile.txt spanishWordlist.txt")
